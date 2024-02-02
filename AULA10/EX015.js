71194b636b5eda4629b3d02147fe98f469b47a40
dionatamartini <dionatamartinii@hotmail.com>
2024-02-02 11:29:01 -0300

    ex015

--- AULA10/EX015.js
+++ AULA10/EX015.js
@@ -3,11 +3,17 @@ var c = 1
 do (c <= 5) {
     console.log (c)
     c++
-} */
+} 
 
 console.log('Vai começar...')
-for(var c = 1; c <= 100; c++) {
+for(var c = 1; c <= 10; c++) {
     console.log(c)
 }
-console.log ('FIM!')
+console.log ('FIM!')*/
+
+var c = 1
+do (c <= 10) {
+    console.log (c)
+    c+2
+}
 
---
+++ AULA10/node_b0e97e8f706cd.tmp.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+do (c <= 5) {
+    console.log (c)
+    c++
+} */
+
+console.log('Vai começar...')
+for(var c = 1; c <= 100; c = 1 + 2) {
+    console.log(c)
+}
+console.log ('FIM!')
+
